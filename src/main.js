1e57859078fb699b372c4fc0ec9070dc4bf058f6
Victor Souza Silveira <victor.silveira@idealtrends.local>
2025-11-25 11:48:14 -0300

    finalizacao da header

--- src/main.js
+++ src/main.js
@@ -1,32 +1,44 @@
+const $ = (selector) => document.querySelector(selector);
 
-
-
-// 2) Victor Souza
 new Typed('#typed1', {
     strings: ['Victor Souza'],
     typeSpeed: 50,
-    backSpeed: 0,
     showCursor: false,
     onComplete: function () {
 
-        // 3) Dev Full Stack
         new Typed('#typed2', {
             strings: ['Dev Full Stack'],
             typeSpeed: 50,
-            backSpeed: 0,
             showCursor: false,
             onComplete: function () {
 
                 new Typed('#typed3', {
                     strings: ['PHP | Java | SpringBoot | MySQL | PostgreSQL'],
                     typeSpeed: 50,
-                    backSpeed: 0,
-                    showCursor: false
+                    showCursor: false,
+                    onComplete: function () {
+
+                        // Botão Currículo
+                        const curriculoDiv = $('.header__actions div:nth-child(1)');
+                        curriculoDiv.classList.remove('ocult');
+                        curriculoDiv.setAttribute('data-usal', 'split-item split-fade-r duration-500');
+
+                        // Botão LinkedIn
+                        const linkedinDiv = $('.header__actions div:nth-child(2)');
+                        linkedinDiv.classList.remove('ocult');
+                        linkedinDiv.setAttribute('data-usal', 'split-item split-fade-l duration-500 delay-100');
+
+                        // Avatar
+                        const avatarWrapper = $('.header__avatar-wrapper');
+                        avatarWrapper.classList.remove('ocult');
+                        avatarWrapper.setAttribute('data-usal', 'blur-3 duration-800 delay-200');
+
+                        if (window.USAL && typeof window.USAL.scan === 'function') {
+                            window.USAL.scan(); 
+                        }
+                    }
                 });
             }
         });
-
     }
 });
-
-
